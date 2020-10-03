fc321ea16988622d069e1c2d860674ab7496271b
Leyang Zou <leyang.zou@student.tdsb.on.ca>
2020-10-03 14:57:45 -0400

    styling changes and documented functions

--- dyndict/test.py
+++ dyndict/test.py
@@ -10,12 +10,18 @@ class TestDynDict(unittest.TestCase):
         del self.dynd
 
     def test_adding_dictionary(self):
+        """
+        Tests basic addition.
+        """
         self.dynd += {'a': 1}
         self.assertEqual(self.dynd, {'a': 1})
         self.dynd += {'b': 2}
         self.assertEqual(self.dynd, {'a': 1, 'b': 2})
 
     def test_adding_nested_dictionary(self):
+        """
+        Tests addition of nested dyndicts.
+        """
         self.dynd += {'a': {}}
         self.assertEqual(self.dynd, {'a': {}})
         self.dynd += {'a': {'b': {}}}
@@ -24,6 +30,9 @@ class TestDynDict(unittest.TestCase):
         self.assertEqual(self.dynd, {'a': {'b': {'c': 3}}})
 
     def test_adding_list(self):
+        """
+        Tests addition of lists.
+        """
         self.dynd += {'a': []}
         self.assertEqual(self.dynd, {'a': []})
         self.dynd += {'a': [1, 2, 3]}
@@ -34,6 +43,9 @@ class TestDynDict(unittest.TestCase):
         self.assertEqual(self.dynd, {'a': [1, 2, 3, {'b': 2}, {'b': 2}]})
 
     def test_adding_set(self):
+        """
+        Tests addition of sets.
+        """
         self.dynd += {'a': set()}
         self.assertEqual(self.dynd, {'a': set()})
         self.dynd += {'a': {1, 2, 3}}
@@ -44,22 +56,34 @@ class TestDynDict(unittest.TestCase):
         self.assertEqual(self.dynd, {'a': {1, 2, 3, 4, 5, 6}})
 
     def test_overwriting_int(self):
+        """
+        Tests the overwriting of integers.
+        """
         self.dynd += {'a': -1}
         self.assertEqual(self.dynd, {'a': -1})
         self.dynd += {'a': 1}
         self.assertEqual(self.dynd, {'a': 1})
 
     def test_overwriting_string(self):
+        """
+        Tests the overwriting of strings.
+        """
         self.dynd += {'a': 'abc'}
         self.assertEqual(self.dynd, {'a': 'abc'})
         self.dynd += {'a': 'def'}
         self.assertEqual(self.dynd, {'a': 'def'})
 
     def test_init_with_dict(self):
+        """
+        Tests the initialization of a dyndict with default values.
+        """
         self.dynd = dyndict({'a': 1})
         self.assertEqual(self.dynd, {'a': 1})
 
     def test_refer_by(self):
+        """
+        Tests refer_by initialization and usage.
+        """
         self.dynd = dyndict(refer_by='name')
         self.dynd += {'list': [{'name': 'a', 'x': 0}, {'name': 'b', 'x': 1}]}
         self.assertEqual(self.dynd, {'list': [{'name': 'a', 'x': 0}, {'name': 'b', 'x': 1}]})
